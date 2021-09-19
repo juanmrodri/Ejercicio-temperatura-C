9874025a409bf7f66c73607315751b93926cc71f
juanRodriguez <juanmrodriguez1989@gmail.com>
2021-09-19 11:03:24 -0300

    agregue comentarios en el el header .h

--- src/utn_biblioteca_arrays.h
+++ src/utn_biblioteca_arrays.h
@@ -11,7 +11,17 @@
 #ifndef UTN_BIBLIOTECA_ARRAYS_H_
 #define UTN_BIBLIOTECA_ARRAYS_H_
 
+	/// @brief void imprimirArray(float[], int)
+	/// imprime todas las posiciones del array, en este caso del tipo float
+	/// @param pArrayUno esta es la direccion de memoria del array
+	/// @param len es la cantidad de posiciones del array
 void imprimirArray(float pArrayUno[], int len);
+
+	/// @brief void inicalizarArray(int[], int)
+	/// inicializa todas las posiciones de un array, en este caso del tipo int
+	/// @param pArrayUno esta es la direccion de memoria del array
+	/// @param len es la cantidad de posiciones del array
+	/// @param valorIninial es el valor que tendra cada posicion del array
 void inicalizarArray(int pArrayUno[], int len, int valorIninial);
 
 
